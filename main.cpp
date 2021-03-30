5133e5d9ee4bad0f8d964acfede27603e04f3046
Kenneth C <kennethalonsoc@gmail.com>
2021-03-29 20:01:22 -0600

    QT GUI Update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
-using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
